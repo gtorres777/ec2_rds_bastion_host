1277e66254c83b2a67f1f994e120ee9987e544d6
tux <gustavo.torres.d@tecsup.edu.pe>
2023-06-27 17:46:11 -0500

    test master

---
+++ environments/root/main.tf
@@ -0,0 +1,14 @@
+module "lambda-dms-stop-tasks" {
+  source = "./modules/lambda"
+
+  source_dir = "lambdas_code/dms_stop_tasks/code"
+  output_path = "lambdas_code/dms_stop_tasks/code.zip"
+  lambda_function_name = "dms_stop_tasks"
+  # filename = "dms_stop_tasks/code/code.zip"
+  filename = "dms_stop_tasks/code.zip"
+  runtime = "python3.8"
+  handler = "main.lambda_handler"
+  policy_name = "dms_stop_tasks_policy"
+  policy_file_name = "dms-stop-tasks.json"
+
+}
---
+++ environments/root/master/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_role_arn="arn:aws:iam::88:role/infrastructure-automation-role"
+vpc_id="vpc-aa9"
