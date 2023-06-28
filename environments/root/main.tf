3a76c4679b59b15d6b2ed3c3f25641b9d832fc0e
tux <gustavo.torres.d@tecsup.edu.pe>
2023-06-27 23:11:12 -0500

    test master39

--- environments/root/main.tf
+++ environments/root/main.tf
@@ -5,7 +5,6 @@ module "lambda-dms-stop-tasks" {
   source_dir = "lambdas_code/dms_stop_tasks/code"
   output_path = "lambdas_code/dms_stop_tasks/code.zip"
   lambda_function_name = "dms_stop_tasks"
-  # filename = "dms_stop_tasks/code/code.zip"
   filename = "dms_stop_tasks/code.zip"
   runtime = "python3.8"
   handler = "main.lambda_handler"
