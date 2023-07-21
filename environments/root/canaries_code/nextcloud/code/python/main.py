4860d6ff6ef5eb73bd80fcba369028b9f587a7c8
tux <gustavo.torres.d@tecsup.edu.pe>
2023-07-20 22:55:38 -0500

    DO-140 - refactorized canary code

--- environments/root/canaries_code/nextcloud/code/python/main.py
+++ environments/root/canaries_code/nextcloud/code/python/main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def nextcloud_health_check(url):
     browser = syn_webdriver.Chrome()
 
