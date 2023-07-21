659b9965bb20e608586dc8bda21ea8d0aa246125
tux <gustavo.torres.d@tecsup.edu.pe>
2023-07-20 22:42:26 -0500

    DO-140 - refactorized canary code

--- environments/root/canaries_code/nextcloud/code/python/main.py
+++ environments/root/canaries_code/nextcloud/code/python/main.py
@@ -1,40 +1,61 @@
 from aws_synthetics.selenium import synthetics_webdriver as syn_webdriver
 from aws_synthetics.common import synthetics_logger as logger
-import boto3
+import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def main():
-
-    url = "https://cloud.gustavo-td.com"
-
-    # Set screenshot option
-    takeScreenshot = True
-
+def nextcloud_health_check(url):
     browser = syn_webdriver.Chrome()
 
     try:
         logger.info("Connecting to URL ...")
         browser.get(url)
 
-        if takeScreenshot:
-            browser.save_screenshot("loaded.png")
+        # Wait for the page to load completely before extracting the page source
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "app-status")))
 
-        response_code = syn_webdriver.get_http_response(url)
+        # Get the page source JSON response
+        page_source = browser.page_source
+
+        # Parse the JSON response
+        json_data = json.loads(page_source)
+
+        # Extract and validate the HTTP status code
+        status_code = browser.execute_script("return window.performance.getEntries()[0].response.status")
+        if status_code == 200:
+            logger.info("HTTP status code is 200 OK.")
+        else:
+            raise Exception(f"HTTP status code is not 200 OK. Status: {status_code}")
+
+        # Extract and validate the installed and maintenance fields in the JSON response
+        installed = json_data.get("installed", False)
+        maintenance = json_data.get("maintenance", False)
+        if installed:
+            logger.info("Nextcloud is installed and healthy.")
+        else:
+            raise Exception("Nextcloud is not installed or not healthy.")
+
+        if maintenance:
+            logger.info("Nextcloud is in maintenance mode.")
+        else:
+            raise Exception("Nextcloud is not in maintenance mode.")
 
         logger.info("Canary successfully executed.")
 
-        if not response_code or response_code < 200 or response_code > 299:
-            raise Exception("Failed to load page!")
-
     except Exception as e:
+        logger.error(f"Failed to load page! ERROR: {str(e)}")
 
-        print(f"ERROR MESSAGE: {str(e)}")
+        # Save a screenshot on failure
+        browser.save_screenshot("loaded.png")
 
-        if takeScreenshot:
-            browser.save_screenshot("loaded.png")
+        raise
 
-        raise Exception(f"Failed to load page! with ERROR {str(e)}")
+    finally:
+        # Close the WebDriver
+        browser.quit()
 
 def handler(event, context):
     logger.info("Selenium Python heartbeat canary.")
-    return main()
-
+    url = "https://cloud.gustavo-td.com/status.php"
+    nextcloud_health_check(url)
