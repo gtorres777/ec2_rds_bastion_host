65421cfa1ddaf85ad1de844ee0d5544a87511f40
tux <gustavo.torres.d@tecsup.edu.pe>
2023-07-20 23:53:52 -0500

    DO-140 - refactorized canary code

--- environments/root/canaries_code/nextcloud/code/python/main.py
+++ environments/root/canaries_code/nextcloud/code/python/main.py
@@ -37,7 +37,7 @@ def nextcloud_health_check(url):
 
 
         # Extract and validate the HTTP status code
-        status_code = browser.execute_script("return window.performance.getEntries()[0].responseStatus")
+        status_code = browser.execute_script("return window.performance.getEntries()[0]?.responseStatus")
         logger.info("RESPONSE STATUS")
         logger.info(status_code)
         if status_code == 200:
