a7f7ea876fe079adfcd26366a08785784df837e0
tux <gustavo.torres.d@tecsup.edu.pe>
2023-07-21 00:27:19 -0500

    DO-140 - refactorized canary code

--- environments/root/canaries_code/nextcloud/code/python/main.py
+++ environments/root/canaries_code/nextcloud/code/python/main.py
@@ -18,7 +18,7 @@ def nextcloud_health_check(url):
         browser.get(url)
 
         # Wait for the page to load completely before extracting the page source
-        WebDriverWait(browser, timeout=15)
+        WebDriverWait(browser, timeout=15).until(browser.execute_script("return window.performance.getEntries()[0]?.responseStatus;") is not None)
 
         logger.info("RECIEN PAGE")
 
