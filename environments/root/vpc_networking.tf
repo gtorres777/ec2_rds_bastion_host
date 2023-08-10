13c79e0df34e2a5a611c3173bc8a1dc3cd2cc1e8
tux <gustavo.torres.d@tecsup.edu.pe>
2023-08-10 12:57:39 -0500

    fixed for subnets

--- environments/root/vpc_networking.tf
+++ environments/root/vpc_networking.tf
@@ -9,7 +9,7 @@ module "vpc_networking" {
   aws_region         = var.aws_region
 
   # vpc_cidr = "11.0.0.0/16"
-  vpc_cidr = "11.0.0.0/16"
+  vpc_cidr = var.vpc_cidr
 
   # public_subnets_cidr      = ["11.0.1.0/24", "11.0.2.0/24"]
   public_subnets_cidr      =  var.public_subnets_cidr
