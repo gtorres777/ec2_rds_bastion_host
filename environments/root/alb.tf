3df3fb5d85ca8ed0163cea6ad4ebde8ace1ce8e5
tux <gustavo.torres.d@tecsup.edu.pe>
2023-08-16 16:57:39 -0500

    testing null final2

--- environments/root/alb.tf
+++ environments/root/alb.tf
@@ -7,3 +7,7 @@ module "alb" {
   subnets_ids     = module.vpc_networking.public_subnets_ids
   certificate_arn   = var.certificate_alb_private_arn   
 }
+
+output "certificatearn" {
+  value = var.certificate_alb_private_arn == null ? "itsnull": var.certificate_alb_private_arn
+}
