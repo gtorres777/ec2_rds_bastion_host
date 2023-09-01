d6817c9d2983684522a62b4a8873f24b873cc95e
tux <gustavo.torres.d@tecsup.edu.pe>
2023-09-01 17:27:46 -0500

    testing for staging csm

---
+++ environments/development/alarm.tf
@@ -0,0 +1,19 @@
+# module "alarm" {
+#   source = "../../modules/alarms"
+
+#   alarm_name                = "Synthetics-Alarm-nextcloud"
+#   comparison_operator       = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods        = 1
+#   metric_name               = "Failed"
+#   namespace                 = "CloudWatchSynthetics"
+#   period                    = 900
+#   statistic                 = "Sum"
+#   threshold                 = 2.0
+#   dimensions_name            = "CanaryName"
+#   dimensions_value           = module.canary.cloudwatch_canary.name
+#   alarm_actions = [module.snstux.aws_sns_topic_arn]
+
+#   depends_on = [
+#     module.canary
+#   ]
+# }
---
+++ environments/experimental/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  default = "us-east-1"
+  type    = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets_cidr" {
+  type = list(string)
+}
+
+variable "certificate_alb_private_arn" {
+  type = any
+
+  validation {
+    condition     = can(tostring(var.certificate_alb_private_arn))
+    error_message = "The \"certificate_alb_private_arn\" value must be a string."
+  }
+}
